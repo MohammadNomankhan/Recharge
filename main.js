bd8953dbd5807d086a65d393ed45447b4151d7fc
MohammadNomankhan <khannoman1702@gmail.com>
2022-10-27 12:56:35 +0530

    completed the music player

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -89,3 +93,48 @@ function nextSong() {
 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+
+
+function updateProgressBar(e) {
+	const {duration, currentTime} = e.srcElement;
+	const liveProgress = (currentTime/duration) * 100;
+	progress.style.width = `${liveProgress}%`
+
+	// update duration
+	const durationMinutes = Math.floor(duration/60);
+	let durationSeconds = Math.floor(duration % 60);
+	if(durationSeconds < 10) {
+		durationSeconds = `0${durationSeconds}`;
+	}
+	if (durationSeconds) {
+		durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+	}
+
+	// update current time
+	let currentMinutes = Math.floor(currentTime/60);
+	let currentSeconds = Math.floor(currentTime % 60);
+
+	if (currentSeconds < 10) {
+		currentSeconds = `0${currentSeconds}`;
+	} 
+	if (currentSeconds) {
+		currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+	}
+}
+
+function setProgressBar(e) {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const {duration} = music;
+	music.currentTime = (clickX / width) * duration;
+}
+
+
+// event listnener for progress bar
+music.addEventListener('timeupdate', updateProgressBar);
+
+// event listener for update current time
+progressContainer.addEventListener('click',setProgressBar);
+
+// event listener for next song when current ends
+music.addEventListener('ended', nextSong);
